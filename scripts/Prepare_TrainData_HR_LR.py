ece24cbd2661264f330abf265b32d1d5d1bac317
Ashish <32778419+ashj9@users.noreply.github.com>
2020-12-01 14:46:43 +0530

    Add files via upload

---
+++ scripts/Prepare_TrainData_HR_LR.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-05-21 19:55
+# @Author  : LeeHW
+# @File    : Prepare_data.py
+# @Software: PyCharm
+from glob import glob
+from flags import *
+import os
+from scipy import misc
+import numpy as np
+import datetime
+import imageio
+from multiprocessing.dummy import Pool as ThreadPool
+import sys
+import cv2
+
+starttime = datetime.datetime.now()
+save_dir = '/scratch/ashj/'
+save_HR_path = os.path.join(save_dir, 'HR_x4')
+save_LR_path = os.path.join(save_dir, 'LR_x4')
+os.system('mkdir -p {}'.format(save_HR_path))
+os.system('mkdir -p {}'.format(save_HR_path))
+train_HR_dir = sys.argv[1]
+# file_list = sorted(glob(os.path.join(train_HR_dir, '*.png')))
+file_list = sorted(glob(os.path.join(train_HR_dir, '*.npy')))
+
+HR_size = [100, 0.8, 0.7, 0.6, 0.5]
+
+
+def save_HR_LR(img, size, path, idx):
+	HR_img = misc.imresize(img, size, interp='bicubic')
+	HR_img = modcrop(HR_img, 4)
+	rot180_img = misc.imrotate(HR_img, 180)
+	x4_img = misc.imresize(HR_img, 1 / 4, interp='bicubic')
+	x4_rot180_img = misc.imresize(rot180_img, 1 / 4, interp='bicubic')
+
+	img_path = path.split('/')[-1].split('.')[0] + '_rot0_' + 'ds' + str(idx) + '.png'
+	rot180img_path = path.split('/')[-1].split('.')[0] + '_rot180_' + 'ds' + str(idx) + '.png'
+	x4_img_path = path.split('/')[-1].split('.')[0] + '_rot0_' + 'ds' + str(idx) + '.png'
+	x4_rot180img_path = path.split('/')[-1].split('.')[0] + '_rot180_' + 'ds' + str(idx) + '.png'
+
+	misc.imsave(save_HR_path + '/' + img_path, HR_img)
+	misc.imsave(save_HR_path + '/' + rot180img_path, rot180_img)
+	misc.imsave(save_LR_path + '/' + x4_img_path, x4_img)
+	misc.imsave(save_LR_path + '/' + x4_rot180img_path, x4_rot180_img)
+
+
+def modcrop(image, scale=4):
+	if len(image.shape) == 3:
+		h, w, _ = image.shape
+		h = h - np.mod(h, scale)
+		w = w - np.mod(w, scale)
+		image = image[0:h, 0:w, :]
+	else:
+		h, w = image.shape
+		h = h - np.mod(h, scale)
+		w = w - np.mod(w, scale)
+		image = image[0:h, 0:w]
+	return image
+
+
+def main(path):
+	print('Processing-----{}/0800'.format(path.split('/')[-1].split('.')[0]))
+	# img = imageio.imread(path)
+	img = np.load(path)
+	idx = 0
+	for size in HR_size:
+		save_HR_LR(img, size, path, idx)
+		idx += 1
+
+items = file_list
+pool = ThreadPool()
+pool.map(main, items)
+pool.close()
+pool.join()
+endtime = datetime.datetime.now()
+print((endtime - starttime).seconds)
+
+
+# if __name__ == "__main__":
+# 	path = sys.argv[1]
+# 	main(path)
