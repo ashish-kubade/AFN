d11f573ac0900046f17637e3b4455728f793839d
Ashish <32778419+ashj9@users.noreply.github.com>
2020-12-02 18:40:07 +0530

    Create README.m

---
+++ datasets/README.m
@@ -0,0 +1,2 @@
+Add your training datasets into respective HR and LR directories. Currently we have provided a small portion of the training dataset used in our research.
+The files train.npy and val.npy enlist the names of the files used for training and validation respectively.
